4794d9d6d6cda217692ad91937c80522020b1376
23DP2DTuma <166400530+23DP2DTuma@users.noreply.github.com>
2025-10-17 18:36:11 +0000

    Done!!

--- Mogo/assets/js/app.js
+++ Mogo/assets/js/app.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     var header = $("header");
     var introH = $("#intro").innerHeight();
@@ -7,14 +7,14 @@ $(function() {
     /* fixed header */
     checkScroll(scrollOffset);
 
-    $(window).on("scroll", function() {
+    $(window).on("scroll", function () {
         scrollOffset = $(this).scrollTop();
         checkScroll(scrollOffset);
 
     });
 
     function checkScroll(scrollOffset) {
-        if ( scrollOffset >= introH) {
+        if (scrollOffset >= introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
@@ -23,15 +23,43 @@ $(function() {
 
     /* smooth scroll */
 
-    $("[data-scroll]").on("click", function(event) {
+    $("[data-scroll]").on("click", function (event) {
         event.preventDefault();
 
-        var elementID = $(this).data('scroll'),
-            blockofset = $(elementID).offset().top;
+        var $this = $(this),
+            blockID = $this.data('scroll'),
+            blockOffset = $(blockID).offset().top;
+
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
 
         $("html, body").animate({
-            scrollTop: blockofset
+            scrollTop: blockOffset
         }, 500);
+    });
+
+    /* Menu nav toogle */
+
+    $("#nav_toogle").on("click", function (event) {
+        event.preventDefault();
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+    /* collapse */
+
+    $("[data-collapse]").on("click", function (event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockID = $this.data('collapse');
+        $(blockID).slideToggle();
+    });
+
+    $("[data-slider]").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
     });
 
 });
